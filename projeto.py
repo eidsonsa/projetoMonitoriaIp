2471c43c1aa4a076d3c3617ad81694171d67fe27
Eidson <ejas@cin.ufpe.br>
2020-02-27 01:39:01 -0300

    leitura de dados

--- projeto.py
+++ projeto.py
@@ -1,1 +1,21 @@
 import matplotlib.pyplot as plt
+
+class Carro:
+
+    def __init__(self, nome, ano, km, cor, portas):
+        this.nome = nome
+        this.ano = ano
+        this.km = km
+        this.cor = cor
+        this.portas = portas
+
+carros = []
+
+for i in range(1, 15):
+    s = 'test' + str(i) + '.txt'
+    carro = open(s, 'r')
+    cabeçalho = carro.readline()
+    # for j, linha in enumerate(carro):
+    nome, cidade, cabAno, ano, cabKm, km, cabCor, cor,  cabPortas, portas, preco, specs = carro.split('\n')
+    carrobj = Carro(nome, ano, km, cor, portas)
+    carros.append(carrobj)  

636b8282b3303652e494c6db14e21c1ba391917d
Eidson <ejas@cin.ufpe.br>
2020-02-29 15:12:36 -0300

    correcao erros plotagem dos graficos corretamente

--- projeto.py
+++ projeto.py
@@ -3,51 +3,70 @@ import matplotlib.pyplot as plt
 class Carro:
 
     def __init__(self, nome, ano, km, cor, portas):
-        this.nome = nome
-        this.ano = ano
-        this.km = km
-        this.cor = cor
-        this.portas = portas
+        self.nome = nome
+        self.ano = ano
+        self.km = km
+        self.cor = cor
+        self.portas = portas
 
 carros = []
 
 for i in range(1, 15):
     s = 'test' + str(i) + '.txt'
-    carro = open(s, 'r')
-    cabeçalho = carro.readline()
-    # for j, linha in enumerate(carro):
-    nome, cidade, cabAno, ano, cabKm, km, cabCor, cor,  cabPortas, portas, preco, specs = carro.split('\n')
+    carro = open(s, 'r', encoding='utf8')
+    linhas = carro.readlines()
+    
+    nome = linhas[0] 
+    ano = None
+    km = None
+    cor = None 
+    portas = None
+    
+    for j in range(0, len(linhas) - 1):
+        if linhas[j] == 'Modelo\n':
+            nome = linhas[j + 1]
+        if linhas[j] == 'Ano\n' or linhas[j] == 'ANO\n':
+            ano = linhas[j + 1]
+        if linhas[j] == 'KM\n' or linhas[j] == 'Quilometragem\n':
+            km = linhas[j + 1]
+        if linhas[j] == 'Cor\n' or linhas[j] == 'COR\n':
+            cor = linhas[j + 1]
+        if linhas[j] == 'Portas\n' or linhas[j] == 'PORTAS\n':
+            portas = linhas[j + 1]
+    
+    if portas is None:
+        portas = 'Nao especificado'
+
+    portas.replace(' portas', '')
     carrobj = Carro(nome, ano, km, cor, portas)
     carros.append(carrobj)  
 
-# tem q testar essa parada e eu acho q tem algo errado entao esperar
-
-nomes = [i.nome for i in carros]
-anos = [i.ano for i in carros]
-kms = [i.km for i in carros]
-cores = [i.cor for i in carros]
-portas = [i.portas for i in carros]
+nomes = [i.nome.replace('\n', '') for i in carros if i.nome is not None]
+anos = [i.ano.replace('\n', '') for i in carros if i.ano is not None]
+kms = [i.km.replace('\n', '') for i in carros if i.km is not None]
+cores = [i.cor.replace('\n', '') for i in carros if i.cor is not None]
+portas = [i.portas.replace(' portas', '').replace('\n', '') for i in carros if i.portas is not None]
 
 plt.plot(anos, nomes)
-plt.title('Anos de lançamento dos carros')
-plt.xlabel('Anos de lançamento')
-plt.ylabel('Carros')
+plt.title('Anos de lançamento')
+plt.xlabel('Ano')
+plt.ylabel('Carro')
 plt.show()
 
 plt.plot(kms, nomes)
-plt.title('Kms rodados dos carros')
-plt.xlabel('Kms rodados')
-plt.ylabel('Carros')
+plt.title('Kms rodados')
+plt.xlabel('Km')
+plt.ylabel('Carro')
 plt.show()
 
 plt.plot(cores, nomes)
-plt.title('Cores dos carros')
-plt.xlabel('Cores')
-plt.ylabel('Carros')
+plt.title('Cores')
+plt.xlabel('Cor')
+plt.ylabel('Carro')
 plt.show()
 
 plt.plot(portas, nomes)
-plt.title('Quantidade de portas dos carros')
+plt.title('Quantidade de portas')
 plt.xlabel('Quantidade de portas')
 plt.ylabel('Carros')
 plt.show()
